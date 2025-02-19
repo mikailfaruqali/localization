208ead8d1c81cd6d385b211acc731b30ba16cc46
Snawbar <alanfaruq85@gmail.com>
2025-02-19 21:06:04 +0300

    Initialize localization package with basic structure and configuration files

---
+++ src/LocalizationServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+namespace Snawbar\Layout;
+
+use Illuminate\Support\ServiceProvider;
+
+class LocalizationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
